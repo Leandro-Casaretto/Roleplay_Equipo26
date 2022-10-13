50be07ded7a81dca0349e1b4d6885416ee3fc014
ffrancodl <104432558+ffrancodl@users.noreply.github.com>
2022-10-13 15:45:38 -0300

    Create Casco_Mágico.cs

---
+++ src/Library/Objetos/Casco_Mágico.cs
@@ -0,0 +1,14 @@
+namespace Library
+{
+    public class cascoMagico
+    {
+        public int valorDefensa
+          {
+            get
+              { 
+                  return 10;
+              }
+           }
+          
+          public int valorAtaque
+            {
