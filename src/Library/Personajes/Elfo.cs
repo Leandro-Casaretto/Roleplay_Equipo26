5bb549619affc9ec584f8c55b7b75cb0d15e9ba6
Leandro-Casaretto <104432732+Leandro-Casaretto@users.noreply.github.com>
2022-10-13 20:49:07 -0300

    Update Elfo.cs

--- src/Library/Personajes/Elfo.cs
+++ src/Library/Personajes/Elfo.cs
@@ -10,7 +10,7 @@ namespace Library
         public int Ataque {get; set;} = 25;
         public int habilidadMagica = 40; /// hechizo que devuelve 20 de vida a integrantes del equipo
         
-        public Elfo (string nombre, int vida, int ataque, int defensa);
+        public Elfo (string nombre, int vida, int ataque, int defensa)
         {
           this.Nombre = nombre;
           this.Vida = vida;
