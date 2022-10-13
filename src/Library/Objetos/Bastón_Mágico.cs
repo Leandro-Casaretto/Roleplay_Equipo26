40950c9ef5f7b1a1c354bb7fc0928caefa7d2675
Guss310 <105060468+Guss310@users.noreply.github.com>
2022-10-13 15:35:04 -0300

    Update Bastón_Mágico.cs

--- src/Library/Objetos/Bastón_Mágico.cs
+++ src/Library/Objetos/Bastón_Mágico.cs
@@ -2,8 +2,8 @@ namespace Library
 {
     public class LibroDeEchizos
     {
-        public int Daño {get; private set;} = 0;
-        public int Defensa {get; private set;} = 0;
+        public int Daño {get; set;} = 0;
+        public int Defensa {get; set;} = 0;
 
         public void NuevoEchizo(Echizo echizo)
         {
