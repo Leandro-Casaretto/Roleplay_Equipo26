7292bdf69d77a4a02f078b2a50e79723e41f8529
ffrancodl <104432558+ffrancodl@users.noreply.github.com>
2022-10-13 15:50:15 -0300

    Update and rename Arco_Encantado to Arco_Encantado.cs

---
+++ src/Library/Objetos/Arco_Encantado.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace Library
+
+{
+  public class arcoEncantado
+  
+    {
+        public int valorAtaque
+        {
+            get
+             {
+                return 25;
+             }
+        }
+       
+        public int valorDefensa
+          {
+            get
+              {
+                  return 0;
+               }
+           }
+           
+                
+     }
+ }
