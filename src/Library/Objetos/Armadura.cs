44698f3edffc5b6f0baa95a32d9c5eefb4286283
Leandro-Casaretto <104432732+Leandro-Casaretto@users.noreply.github.com>
2022-10-13 15:36:56 -0300

    Update Armadura.cs

--- src/Library/Objetos/Armadura.cs
+++ src/Library/Objetos/Armadura.cs
@@ -1,1 +1,22 @@
 
+namespace Library
+{
+    public class Armadura
+    {
+        public int ValorDefensa
+        {
+            get
+            {
+                return 20;
+            }
+        }
+    
+        public int ValorAtaque
+        {
+            get
+            {
+                return 0;  /// La armadura no ataca
+            }
+    
+    }
+}
