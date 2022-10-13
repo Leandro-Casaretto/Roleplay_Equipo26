8b89936163bc2e6ff4c6e3037d6d87614ea5b207
Leandro-Casaretto <104432732+Leandro-Casaretto@users.noreply.github.com>
2022-10-13 15:04:50 -0300

    Update Enano.cs

--- src/Library/Personajes/Enano.cs
+++ src/Library/Personajes/Enano.cs
@@ -1,1 +1,28 @@
+using System;
 
+
+namespace Library
+{
+    public class Enano
+    {
+        public string Nombre { get; set; }
+        
+        public int Vida = 130;
+        
+        public int Ataque { get; set; } = 15;
+        
+        public int Defensa = 40;
+        
+        
+         public Enano (string nombre, int vida, int ataque, int defensa) 
+        {
+            this.Nombre = nombre;
+            this.Vida = vida;
+            this.Ataque = ataque;
+            this.Defensa = defensa;
+            
+        }
+        
+        
+        
+      
