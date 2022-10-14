7c32777634888b78c7995e3e9178e6cfb692b844
Leandro-Casaretto <104432732+Leandro-Casaretto@users.noreply.github.com>
2022-10-13 21:52:23 -0300

    Update Objeto.cs

--- src/Library/Objetos/Objeto.cs
+++ src/Library/Objetos/Objeto.cs
@@ -1,1 +1,20 @@
+using System;
 
+namespace Library
+{
+    public class Objeto
+    {
+        public int valorAtaque { get; set; } = 0; 
+        
+        public int valorDefensa { get; set; } = 0;
+        
+        public int Resistencia { get; set; } = 0;
+        
+        // A continuación le damos valor a los atributos
+
+        public Item(int valor_ataque, int valor_defensa, int resistencia)
+        {
+            this.valorAtaque = valor_ataque;
+            this.valorDefensa = valor_defensa;
+            this.Resistencia = resistencia;
+        }
